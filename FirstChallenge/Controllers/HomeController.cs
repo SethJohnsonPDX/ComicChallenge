82c2ceaae6532b6e0af53354f4c915e19474feb3
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-06-06 22:19:49 -0700

    commit all

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comicBooks = FirstChallenge.Models.ComicBookManager.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        // GET: Details
+        public ActionResult Details(int id)
+        {
+            var comicBooks = FirstChallenge.Models.ComicBookManager.GetComicBooks();
+            var comic = comicBooks.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
